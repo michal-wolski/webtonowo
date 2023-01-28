6753c9e7425a294a87988a878df9bf9be1670c8b
Michał Wolski <michal.wolski130@gmail.com>
2023-01-28 22:57:46 +0100

    Main page styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
+import css from "./Footer.module.css";
+
 const Footer = () => {
-  return <div>Webtonowo 2023© Michał Wolski</div>;
+  return (
+    <div className={css.FooterContainer}>Webtonowo 2023© Michał Wolski</div>
+  );
 };
 
 export default Footer;
