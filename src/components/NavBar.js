1e3e038b026aaaa1a28f86a3fffe45c185ed0df2
Michał Wolski <michal.wolski130@gmail.com>
2023-01-29 17:29:10 +0100

    New navbar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,9 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className={css.Nav}>
+      <NavLink className={css.Logo} to="/">
+        Webtonowo
+      </NavLink>
       <nav className={css.NavList}>
         <NavLink className={css.NavListItem} to="/">
           Home
